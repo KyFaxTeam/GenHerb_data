20e4faca96ce1237b6a5b8a33c106ed4a4c9a7db
elonmj <josaphatahouanye@gmail.com>
2024-02-05 10:25:54 +0100

    Ajout de 150 questions au fichier cinéma. Total = 196

--- classement.py
+++ classement.py
@@ -4,26 +4,27 @@
 
 import os
 from unidecode import unidecode
-dossier_matchs = r"C:\Users\Josué\Desktop\Data_geh"
+lien = r"C:\Users\Josué\Desktop\Data_geh"
+dossier_matchs = os.path.join(lien, "Quiz")
 
-
-dossier_resultat = os.path.join(dossier_matchs, "Result")
+dossier_resultat = os.path.join(lien, "Result")
 
 if not os.path.exists(dossier_resultat):
     print("ok")
     os.makedirs(dossier_resultat)
 
 
-rubriques = ["éclairs", "cantonnade", "identification", "langage", 
-             "géographie", "mots croisés", "sport", "cinéma", 
-             "association", "musique", "décentralisation", "relais"]
+rubriques = ["eclairs", "cantonnade", "identification", "langage", 
+             "geographie", "motscroises", "sport", "cinema", 
+             "association", "musique", "decentralisation",
+             "relais", "manchette", "francophonie"]
 shift = max([len(a) for a in rubriques]) + 3
 
 def write_rubrique(nom_rubrique, questions_rubrique):
     chemin_fichier = os.path.join(dossier_resultat, f"{nom_rubrique}.txt")
     mode_ouverture = "a" if os.path.exists(chemin_fichier) else "w"
-    with open(chemin_fichier, mode_ouverture) as fichier:
-        fichier.write("\n".join(questions_rubrique))
+    with open(chemin_fichier, mode_ouverture, encoding ="utf-8") as fichier:
+        fichier.write("\n".join(questions_rubrique)+"\n")
         
 
 def epuration(chaine):
@@ -31,10 +32,13 @@ def epuration(chaine):
                                     "").replace("–", "").replace("-", ""))
 
 def search(ligne):
-    pris = epuration(ligne).replace("cassetete", 
-            "identification").replace("language", "langage").replace("mot",
-            "mots").replace("music", "musique").replace("relai",
-            "relais").replace("cantonade", "cantonnade")[0:shift]
+    pris = epuration(ligne).replace("cassetete", "identification").replace("language",
+            "langage").replace("motscroise", "motscroises").replace("motcroises",
+            "motscroises").replace("music",
+            "musique").replace("relai","relais").replace("cantonade",
+            "cantonnade").replace("eclair", "eclairs").replace("sports",
+            "sport").replace("quiquequoi", "motscroises").replace("cinematographique",
+            "...")[0:shift]
     if pris =="":
         return None
     
@@ -42,8 +46,8 @@ def search(ligne):
 
     joyau = None
     for rubrique in rub:
-        if pris[0:len(rubrique)-1] in rubrique:
-            print("\ni have found", rubrique,"in: ", ligne, "\n", "pris ==", pris)
+        if pris[0:len(rubrique)] ==  rubrique:
+            print("i have found", rubrique,"in:\n ", ligne)#, "\n", "pris ==", pris)
             joyau = rubrique
 
             break
@@ -51,11 +55,14 @@ def search(ligne):
 
 
 for fichier_match in os.listdir(dossier_matchs):
-    if fichier_match == "Result":
+    if not fichier_match.endswith(".txt"):
+        print("FICHIER  à ne pas visiter:", fichier_match)
         continue
+
+    print("..............", fichier_match, "..............")    
     chemin_match = os.path.join(dossier_matchs, fichier_match)
     
-    with open(chemin_match, "r") as match:
+    with open(chemin_match, "r", encoding = 'utf-8') as match:
         lignes = match.readlines()
         
         rubrique_en_cours = None
@@ -65,9 +72,8 @@ for fichier_match in os.listdir(dossier_matchs):
             
             joyau = search(ligne)
             
-            if joyau is not None:
+            if joyau is not None :
                 if len(questions_rubrique)>0:
-                    print("ok")
                     write_rubrique(rubrique_en_cours, questions_rubrique)
 
                 rubrique_en_cours = joyau
